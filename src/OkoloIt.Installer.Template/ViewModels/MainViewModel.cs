3509f42a8d2e8010e17cc86884038e78b1080d14
Ihor <egorwolf2k18@mail.ru>
2024-11-26 19:54:51 +0300

    feat: реализован интерфейс главного окна

--- src/OkoloIt.Installer.Template/ViewModels/MainViewModel.cs
+++ src/OkoloIt.Installer.Template/ViewModels/MainViewModel.cs
@@ -1,9 +1,28 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using OkoloIt.Installer.Template.ViewModels.Pages;
 
 namespace OkoloIt.Installer.Template.ViewModels;
 
 internal sealed partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public string title = "Заголовок";
+    private ViewModelBase _currentPage = new WelcomePageViewModel();
+
+    [ObservableProperty]
+    private bool _isMaximizing;
+
+    [ObservableProperty]
+    private string _programName = "OkoloIt.Installer 2.0.0";
+
+    [RelayCommand]
+    private void OnClose()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            desktop.Shutdown();
+    }
 }
